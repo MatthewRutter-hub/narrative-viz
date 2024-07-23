21767e90a59e3bcb25da7a5988ac61f95d7c54f9
myrutter <myr2@illinois.edu>
2024-07-22 22:59:01 -0700

    Scatter plot added

--- scripts.js
+++ scripts.js
@@ -1,71 +1,53 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Website loaded');
-
-    // Sample data
-    const data = [
-        { name: 'A', value: 30 },
-        { name: 'B', value: 80 },
-        { name: 'C', value: 45 },
-        { name: 'D', value: 60 },
-        { name: 'E', value: 20 },
-        { name: 'F', value: 90 },
-        { name: 'G', value: 50 },
-    ];
-
-    // Set up the SVG and margins
-    const margin = { top: 20, right: 30, bottom: 40, left: 40 };
-    const width = 600 - margin.left - margin.right;
-    const height = 400 - margin.top - margin.bottom;
-
-    const svg = d3.select('#chart')
-        .append('svg')
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
-        .append('g')
-        .attr('transform', `translate(${margin.left},${margin.top})`);
-
-    // Set up the scales
-    const x = d3.scaleBand()
-        .domain(data.map(d => d.name))
-        .range([0, width])
-        .padding(0.1);
-
-    const y = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.value)])
-        .nice()
-        .range([height, 0]);
-
-    // Add the bars
-    svg.selectAll('.bar')
-        .data(data)
-        .enter()
-        .append('rect')
-        .attr('class', 'bar')
-        .attr('x', d => x(d.name))
-        .attr('y', d => y(d.value))
-        .attr('width', x.bandwidth())
-        .attr('height', d => height - y(d.value))
-        .attr('fill', 'steelblue');
-
-    // Add the x-axis
-    svg.append('g')
-        .attr('class', 'x-axis')
-        .attr('transform', `translate(0,${height})`)
-        .call(d3.axisBottom(x));
-
-    // Add the y-axis
-    svg.append('g')
-        .attr('class', 'y-axis')
-        .call(d3.axisLeft(y));
-
-    async function init() {
-        console.log('init called');
-        const data = await d3.csv('cars2017.csv');
-        d3.select('body')
-            .selectAll('p')
-            .data(data)
-            .enter()
-            .append('p')
-            .html(function(d,i) {return 'item ' + d.item + 'is ' + d.number + '.'; });
+async function init() {
+    console.log('Init function called');
+    try {
+        // Load the CSV data
+        const data = await d3.csv('https://flunky.github.io/cars2017.csv');
+        console.log('Data loaded:', data);
+        
+        // Make scatterplot
+        var svg = d3.select("svg"),
+            margin = 50,
+            width = svg.attr("width") + 2 * margin,
+            height = svg.attr("height") + 2 * margin;
+    
+        var x = d3.scaleLog()
+                  .domain([10, 150])
+                  .range([0, 200]);
+    
+        var y = d3.scaleLog()
+                  .domain([10, 150])
+                  .range([200, 0]);
+    
+        var g = svg.append("g")
+                   .attr("transform", "translate(" + margin + "," + margin + ")");
+        
+    
+        g.selectAll("circle")
+         .data(data)
+         .enter().append("circle")
+         .attr("cx", d => x(d.AverageCityMPG))
+         .attr("cy", d => y(d.AverageHighwayMPG))
+         .attr("r", function(d){return 2 + (+d.EngineCylinders);});
+    
+        var xAxis = d3.axisBottom(x)
+                    .tickValues([10, 20, 50, 100])
+                    .tickFormat(d3.format("~s"));
+    
+        var yAxis = d3.axisLeft(y)
+                      .tickValues([10, 20, 50, 100])
+                      .tickFormat(d3.format("~s") );
+    
+        svg.append("g")
+            .attr("transform", "translate(50,50)")
+            .call(yAxis);
+        
+        svg.append("g")
+            .attr("transform", "translate(50,250)")
+            .call(xAxis);
+    } catch (error) {
+        console.error('Error loading data:', error);
     }
-});
+}
+
+//document.addEventListener('DOMContentLoaded', init);
