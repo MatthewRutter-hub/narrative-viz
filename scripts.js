ad1ea30bbe74e4603534dadd6fed621f3e0c44e5
myrutter <myr2@illinois.edu>
2024-07-22 22:11:14 -0700

    Template stuff

---
+++ scripts.js
@@ -0,0 +1,60 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Website loaded');
+
+    // Sample data
+    const data = [
+        { name: 'A', value: 30 },
+        { name: 'B', value: 80 },
+        { name: 'C', value: 45 },
+        { name: 'D', value: 60 },
+        { name: 'E', value: 20 },
+        { name: 'F', value: 90 },
+        { name: 'G', value: 50 },
+    ];
+
+    // Set up the SVG and margins
+    const margin = { top: 20, right: 30, bottom: 40, left: 40 };
+    const width = 600 - margin.left - margin.right;
+    const height = 400 - margin.top - margin.bottom;
+
+    const svg = d3.select('#chart')
+        .append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', `translate(${margin.left},${margin.top})`);
+
+    // Set up the scales
+    const x = d3.scaleBand()
+        .domain(data.map(d => d.name))
+        .range([0, width])
+        .padding(0.1);
+
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.value)])
+        .nice()
+        .range([height, 0]);
+
+    // Add the bars
+    svg.selectAll('.bar')
+        .data(data)
+        .enter()
+        .append('rect')
+        .attr('class', 'bar')
+        .attr('x', d => x(d.name))
+        .attr('y', d => y(d.value))
+        .attr('width', x.bandwidth())
+        .attr('height', d => height - y(d.value))
+        .attr('fill', 'steelblue');
+
+    // Add the x-axis
+    svg.append('g')
+        .attr('class', 'x-axis')
+        .attr('transform', `translate(0,${height})`)
+        .call(d3.axisBottom(x));
+
+    // Add the y-axis
+    svg.append('g')
+        .attr('class', 'y-axis')
+        .call(d3.axisLeft(y));
+});
