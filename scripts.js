9479aa34396e11082e5ea41adb20733828c4300e
myrutter <myr2@illinois.edu>
2024-07-23 23:29:38 -0700

    Tooltip and buttons

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ async function init() {
         
         // Make scatterplot
         var svg = d3.select("svg"),
-            margin = 50,
+            margin = 100,
             width = svg.attr("width") - 2 * margin,
             height = svg.attr("height") - 2 * margin;   
 
@@ -22,14 +22,32 @@ async function init() {
     
         var g = svg.append("g")
                    .attr("transform", "translate(" + margin + "," + margin + ")");
+                   
+        var tooltip = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
         
-    
         g.selectAll("circle")
          .data(data)
          .enter().append("circle")
          .attr("cx", d => x(d.AverageCityMPG))
          .attr("cy", d => y(d.AverageHighwayMPG))
-         .attr("r", function(d){return 2 + (+d.EngineCylinders);});
+         .attr("r", function(d){return 2 + (+d.EngineCylinders);})
+         .on("mouseover", function(event, d) {
+            tooltip.transition()
+                   .duration(200)
+                   .style("opacity", .9);
+            tooltip.html("Make: " + d.Make)
+                   .style("left", (event.pageX + 5) + "px")
+                   .style("top", (event.pageY - 28) + "px");
+            })
+            .on("mouseout", function(d) {
+                tooltip.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            })
+            .append("title")
+            .text(d => `City MPG: ${d.AverageCityMPG}\nHighway MPG: ${d.AverageHighwayMPG}\nCylinders: ${d.EngineCylinders}`);
     
         var xAxis = d3.axisBottom(x)
                     .tickValues([10, 20, 50, 100])
@@ -38,16 +56,40 @@ async function init() {
         var yAxis = d3.axisLeft(y)
                       .tickValues([10, 20, 50, 100])
                       .tickFormat(d3.format("~s") );
-    
-        svg.append("g")
-            .attr("transform", "translate(50,50)")
-            .call(yAxis);
-        var h = height+50
-        svg.append("g")
-            .attr("transform", "translate(50," + h + ")")
-            .call(xAxis);
+
+        g.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+          .append("text")
+            .attr("y", margin - 20)
+            .attr("x", width / 2)
+            .attr("text-anchor", "middle")
+            .attr("stroke", "black")
+            .text("Average City MPG");
+        
+        // Add the Y Axis
+        g.append("g")
+            .call(yAxis)
+          .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0)
+            .attr("x", -height / 2)
+            .attr("dy", "-5.1em")
+            .attr("text-anchor", "middle")
+            .attr("stroke", "black")
+            .text("Average Highway MPG");
 
     } catch (error) {
         console.error('Error loading data:', error);
     }
 }
+
+document.getElementById('toScene1').addEventListener('click', () => {
+    window.location.href = 'index.html';
+ });
+document.getElementById('toScene2').addEventListener('click', () => {
+    window.location.href = 'scene2.html';
+});
+document.getElementById('toScene3').addEventListener('click', () => {
+   window.location.href = 'scene3.html';
+});
